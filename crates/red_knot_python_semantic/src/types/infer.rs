a2d1f2ad567cbc4b6b9cdffdb096f5279c7e8282
Philipp Moritz <pcmoritz@gmail.com>
2024-12-29 12:30:19 -0800

    update

--- crates/red_knot_python_semantic/src/types/infer.rs
+++ crates/red_knot_python_semantic/src/types/infer.rs
@@ -4776,7 +4776,8 @@ impl<'db> TypeInferenceBuilder<'db> {
             }
             _ => {
                 self.infer_type_expression(slice);
-                todo_type!(format!("generics: {:?}", value_ty))
+                println!("generics: {:?}", value_ty);
+                todo_type!("generics")
             }
         }
     }
